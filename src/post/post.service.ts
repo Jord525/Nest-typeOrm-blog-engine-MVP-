a12570869a7d552f0cb7af30a8c8b61bc8e2ebaa
egor dorosenko <dorohenkojord@gmail.com>
2022-04-30 17:53:45 +0300

    add counter for views add search for posts(new, popular)

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
+import { SearchPostDto } from './dto/search-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostEntity } from './entities/post.entity';
 
@@ -16,16 +17,72 @@ export class PostService {
   }
 
   findAll() {
-    return this.repository.find();
+    return this.repository.find({
+      order: {
+        createAt: 'DESC',
+      },
+    });
+  }
+
+  async popular() {
+    const qb = this.repository.createQueryBuilder();
+
+    qb.orderBy('views', 'DESC');
+    qb.limit(10);
+    const [items, total] = await qb.getManyAndCount();
+
+    return {
+      items,
+      total,
+    };
+  }
+
+  async search(dto: SearchPostDto) {
+    const qb = await this.repository.createQueryBuilder('p');
+
+    qb.limit(dto.limit || 0);
+    qb.take(dto.take);
+
+    if (dto.views) {
+      qb.orderBy('views', dto.views);
+    }
+
+    if (dto.body) {
+      qb.andWhere(`p.body ILIKE :body`);
+    }
+
+    if (dto.title) {
+      qb.andWhere(`p.title ILIKE :title`);
+    }
+
+    if (dto.tag) {
+      qb.andWhere(`p.tag ILIKE :tag`);
+    }
+
+    qb.setParameters({
+      title: `%${dto.title}%`,
+      body: `${dto.body}`,
+      tag: `%${dto.tag}%`,
+      views: dto.views || 'DESC',
+    });
+
+    const [items, total] = await qb.getManyAndCount();
+
+    return { items, total };
   }
 
   async findOne(id: number) {
-    const find = await this.repository.findOne(+id);
+    const qb = await this.repository.createQueryBuilder('posts');
 
-    if (!find) {
-      throw new NotFoundException('Статья не найдена');
-    }
-    return find;
+    await qb
+      .whereInIds(id)
+      .update()
+      .set({
+        views: () => 'views + 1',
+      })
+      .execute();
+
+    return this.repository.findOne(id);
   }
 
   async update(id: number, dto: UpdatePostDto) {
